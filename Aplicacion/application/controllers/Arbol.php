c6a2cdff363867912e14430f7365e9f3c617a7d9
JonaJair <jonathan_jair_01@hotmail.com>
2018-06-30 20:20:27 -0500

    Modifiqué la variable global base_url. Controlador: Arbol...agregué los metodos: addTree, deleteTree, vaciarCanaste, misAdopciones; los cuales son para la funcionalidad de la libreria cart.

--- Aplicacion/application/controllers/Arbol.php
+++ Aplicacion/application/controllers/Arbol.php
@@ -4,24 +4,28 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 /**
 * Clase con las funciones que permiten gestionar el módulo de Arbol
 * En esta clase se encuentran métodos como:
-*     - getArbolesTemp()
-*     - getAllInvernadero()
-*     - cargarVistaFront()
+*     - index
+*     - addTree
+*     - deleteTree
+*     - vaciarCanasta
+*     - misAdopciones
+*     - cargarVistaFront
 *
-* @autor Jonathan Jair Alfaro Sánchez
-* @link [dirección_url_de_la_ubicacion]
+* @author Jonathan Jair Alfaro Sánchez
+* @link https://github.com/JOMIMAN-Solutions/MEPPP/tree/master/Aplicacion/application/controllers
 * @package applications/controllers
 *
-* @version 1.0
+* @version 1.0.0
 * Creado el 14/06/2018 a las 10:40 pm
+* Ultima modificacion el 29/06/2018 a las 02:36 pm
 *
-* @since Clase disponible desde la versión 1.0
-* @deprecated Clase obsoleta en la versión 2.0
+* @since Clase disponible desde la versión 1.0.0
+* @deprecated Clase obsoleta en la versión 2.0.0
 * @todo Realizar método para guardar,modificar y eliminar un arbol.
 */
 class Arbol extends CI_Controller 
 {
-	function __construct() {
+    function __construct() {
         parent::__construct();
         $this->load->model('Mdl_Arbol');
     }
@@ -40,20 +44,152 @@ class Arbol extends CI_Controller
     */
     public function index()
     {
-    	$data['title']="MEPPP | Invernadero";
+        $data['title']="MEPPP | Invernadero";
         $data['page']="Invernadero";
         $data['seccion']="1";
         $data['imagen']='invernaderoSeccion';
-        $data['arboles'] = $this->Mdl_Arbol->getAllInvernadero();
+        //Paginación
+        $config['base_url'] = base_url() . 'Arbol/page/';
+        $config['total_rows'] = $this->Mdl_Arbol->totalRows();
+        $config['per_page'] = 2;
+        $config['num_links'] = 4;
+        $config['first_link'] = '<span class="fa fa-angle-double-left"></span>';
+        $config['last_link'] = '<span class="fa fa-angle-double-right"></span>';
+        $config['cur_tag_open'] = '<span class="active">';
+        $config['cur_tag_close'] = '</span>';
+        $config['next_link'] = '<span class="fa fa-angle-right"></span>';
+        $config['prev_link'] = '<span class="fa fa-angle-left"></span>';
+        $config['use_page_numbers'] = TRUE;
+        $this->pagination->initialize($config);
+
+        //$data['arboles'] = $this->Mdl_Arbol->getAllInvernadero();
+        $data["arboles"] = $this->Mdl_Arbol->getTreesPaged($config['per_page'], $this->uri->segment(3));
 
         $this->cargarVistaFront('vw_invernadero',$data);
     }
+
+    /**
+    * Descripción corta del fichero (obligatoria, una línea)
+    * Descripción larga del fichero (opcional, líneas que necesarias)
+    *
+    * @access public
+    * @param 
+    *    - [tipo] [nombre_de_variable] [descripción]
+    *    - [tipo] [nombre_de_variable] [descripción]
+    * @return [tipo_de_dato]
+    *
+    * @since Método disponible desde la versión 1.0
+    * @deprecated Método obsoleto en la versión 2.0
+    * @todo [información]
+    */
+    public function addTree()
+    {
+        $id = $this->input->post('id');
+        $arbolResult = $this->Mdl_Arbol->getOneById($id);
+        foreach ($arbolResult as $arbol) {}
+        $cantidad = 1;
+        
+        // Creamos un arreglo con los productos para insertarlos en el carrito
+        $insert = array(
+            'id' => $id,
+            'qty' => $cantidad,
+            'price' => 0,
+            'name' => $arbol->nombreComun
+        );
+
+        // Insertamos al carrito
+        $this->cart->insert($insert);
+
+        // Obtenemos la url para redirigir a la página en la que estabamos
+        $uri = $this->input->post('uri');
+        
+        /* Redirigimos mostrando un mensaje con las sesiones flashdata
+           confirmando que hemos agregado el árbol */
+        //$this->session->set_flashdata('agregado', 'El árbol fue agregado correctamente');
+        redirect(base_url().'Arbol/page/'.$uri, 'refresh');
+    }
+    
+    /**
+    * Descripción corta del fichero (obligatoria, una línea)
+    * Descripción larga del fichero (opcional, líneas que necesarias)
+    *
+    * @access public
+    * @param 
+    *    - [tipo] [nombre_de_variable] [descripción]
+    *    - [tipo] [nombre_de_variable] [descripción]
+    * @return [tipo_de_dato]
+    *
+    * @since Método disponible desde la versión 1.0
+    * @deprecated Método obsoleto en la versión 2.0
+    * @todo [información]
+    */
+    public function deleteTree($rowid) 
+    {
+        /* Para eliminar un producto debemos conseguir su id y actualizarlo poniendo qty en 0 */
+        $arbol = array(
+            'rowid' => $rowid,
+            'qty' => 0
+        );
+
+        /* después utilizamos la función update para actualizar el carrito pasando el array a actualizar */
+        $this->cart->update($arbol);
+        
+        //$this->session->set_flashdata('productoEliminado', 'El producto fue eliminado correctamente');
+        if ($this->cart->contents()) {
+            redirect(base_url().'Arbol/misAdopciones', 'refresh');
+        } else {
+            redirect(base_url().'Arbol', 'refresh');
+        }
+        
+    }
+    
+    /**
+    * Descripción corta del fichero (obligatoria, una línea)
+    * Descripción larga del fichero (opcional, líneas que necesarias)
+    *
+    * @access public
+    * @param 
+    *    - [tipo] [nombre_de_variable] [descripción]
+    *    - [tipo] [nombre_de_variable] [descripción]
+    * @return [tipo_de_dato]
+    *
+    * @since Método disponible desde la versión 1.0
+    * @deprecated Método obsoleto en la versión 2.0
+    * @todo [información]
+    */
+    public function vaciarCanasta() {
+        $this->cart->destroy();
+        //$this->session->set_flashdata('destruido', 'El carrito fue eliminado correctamente');
+        redirect(base_url().'Arbol', 'refresh');
+    }
+
+    /**
+    * Método que carga los arboles que has agregado a tu "carrito"
+    * Descripción larga del fichero (opcional, líneas que necesarias)
+    *
+    * @access public
+    * @param Ninguno
+    * @return void
+    *
+    * @since Método disponible desde la versión 1.0
+    * @deprecated Método obsoleto en la versión 2.0
+    * @todo [información]
+    */
+    public function misAdopciones()
+    {
+        $data['title']="MEPPP | Mis Adopciones";
+        $data['page']="misAdopciones";
+        $data['seccion']="9";
+        $data['imagen']='invernaderoSeccion';
+
+        $this->cargarVistaFront('vw_misAdopciones',$data);
+    } 
 
 
 
 /* ------------------------------------------------------------------------------------- */
 
-	/**
+    /**
     * Método que carga las vistas especificadas para el frontend
     * Por defecto ya carga el header y footer y la vista a cargar debe ser especificada.
     *
